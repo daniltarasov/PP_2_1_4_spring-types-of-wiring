cdef418db5f6c7b3b6b3d4b256df72be65b35263
ADMIN <daniltarasov@yandex.ru>
2022-12-16 15:14:15 +0300

    answer

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,13 @@
 package koschei.models;
 
+import org.springframework.scheduling.concurrent.DefaultManagedAwareThreadFactory;
+
 public class Needle7 {
+    private Deth8 deth;
+
+    public Needle7(Deth8 deth) {
+        this.deth=deth;
+    }
 
     @Override
     public String toString() {
